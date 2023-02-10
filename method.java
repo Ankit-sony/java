f18207387591b10795c8403787ae2162f3231aab
Ankit Soni <98032129+Ankit-sony@users.noreply.github.com>
2023-02-10 10:58:35 +0000

    method 2

--- method.java
+++ method.java
@@ -33,14 +33,97 @@
 // --------------------------------------java parameters----------------------
 
 
-public class method{
-    static void myMethod(String fname){
-        System.out.println(fname +" Superstar");
+// public class method{
+//     static void myMethod(String fname){
+//         System.out.println(fname +" Superstar");
+//     }
+
+//     public static void main(String[] args){
+//         myMethod("Danish");
+//     }
+// }
+
+
+
+// --------------------------------------java multiple parameters----------------------
+
+
+// public class method{
+//     static void multimethod(String fname, int age){
+//         System.out.println(fname +" is "+ age);
+//     }
+
+//     public static void main(String[] args){
+//         multimethod("Danish",20);
+//         multimethod("Mamav",20);
+//         multimethod("Anushka",20);
+//         multimethod("Anshita",20);
+//         multimethod("Raghaw",20);
+//     }
+// }
+
+
+
+// --------------------------------------return values----------------------
+// public class method{
+//     static int mymethod(int x){
+//     return x + 5;
+//     }
+//     public static void main(String[] args){
+//         System.out.println(mymethod(11));
+//     }
+//     }
+
+
+
+
+
+ // --------------------------------------return 2 values----------------------   
+
+//  public class method{
+//     static int mymethod(int x, int y){
+//         return x + y;
+//     }
+
+//     public static void main(String[] args){
+//         System.out.println(mymethod(12,7));
+//     }
+//  }
+
+  // --------------------------------------return 2 values and store in variable----------------------  
+
+
+//   public class method{
+//     static int mymethod(int x, int y){
+//         return x + y;
+//     }
+
+//     public static void main (String[] args){
+//         int z = mymethod(5, 3);
+//         System.out.println(z);
+//     }
+//   }
+
+
+
+
+  // --------------------------------------if else----------------------  
+
+
+  public class method{
+    static void myage(int age){
+          if(age<18){
+          System.out.println("you can't vote");
+            }
+          else{ 
+            System.out.println("you can vote");
+            }
+
     }
 
     public static void main(String[] args){
-        myMethod("Danish");
+        myage(25);
     }
-}
+  }
 
 
---
+++ method1.java
@@ -0,0 +1,16 @@
+public class method1 {
+    static int mymethodint(int x, int y){
+            return x + y;
+    }
+    static double mymethoddouble(double x, double y){
+        return x + y;
+}
+public static void main(String[] args){
+    int mynum1= mymethodint(12, 1234);
+    double mynum2= mymethoddouble(12.234, 1.234);
+
+    System.out.println("int: " +mynum1);
+    System.out.println("double: " +mynum2);
+}
+    
+}
