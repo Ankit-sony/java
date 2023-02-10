ea2adb2249b99c8e38806b44240a838fcd5029ba
Ankit Soni <98032129+Ankit-sony@users.noreply.github.com>
2023-02-10 21:24:20 +0000

    method22

---
+++ method2.java
@@ -0,0 +1,23 @@
+// public class method2 {
+    
+//     public static void main(String[] args){
+//         //code here cannot use x
+//         int x = 100;
+//         //code here can use x
+//         System.out.println(x);
+//     }
+
+// }
+
+public class method2{
+    public static void main(String[] args){
+        //code here cannot us x
+        {
+            int x = 200;
+            //code here can use x
+            System.out.println(x);
+        }
+        //code here cannot use x
+        // System.out.println(x); this will not work
+    }
+}
