9054a7ab5f258f33b7badb74e084c90dfb607d6b
Ankit Soni <98032129+Ankit-sony@users.noreply.github.com>
2023-02-10 09:52:00 +0000

    method

--- method.java
+++ method.java
@@ -1,1 +1,24 @@
+// public class method {
+//   static void myMethod() {
+//     System.out.println("first method");
+//   }
 
+//   public static void main(String[] args) {
+//     myMethod();
+//   }
+// }
+public class method{
+    static void secondMethod() {
+        System.out.println("second method");
+    }
+    static void thirdMethod(){
+        System.out.println("thirdmethod");
+
+    }
+
+    public static void main(String[] args){
+        secondMethod();
+        thirdMethod();
+    }
+
+}
