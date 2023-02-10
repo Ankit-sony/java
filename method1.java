61831ce5eb85c6530fa35c5e95c1bb745a13e4f9
Ankit Soni <98032129+Ankit-sony@users.noreply.github.com>
2023-02-10 11:13:53 +0000

    method 2 2

--- method1.java
+++ method1.java
@@ -1,16 +1,36 @@
+// public class method1 {
+//     static int mymethodint(int x, int y){
+//             return x + y;
+//     }
+//     static double mymethoddouble(double x, double y){
+//         return x + y;
+// }
+// public static void main(String[] args){
+//     int mynum1= mymethodint(12, 1234);
+//     double mynum2= mymethoddouble(12.234, 1.234);
+
+//     System.out.println("int: " +mynum1);
+//     System.out.println("double: " +mynum2);
+// }
+    
+// }
+
+
+
 public class method1 {
-    static int mymethodint(int x, int y){
+    static int mymethod(int x, int y){
             return x + y;
     }
-    static double mymethoddouble(double x, double y){
+    static double mymethod(double x, double y){
         return x + y;
 }
 public static void main(String[] args){
-    int mynum1= mymethodint(12, 1234);
-    double mynum2= mymethoddouble(12.234, 1.234);
+    int mynum1= mymethod(12, 1234);
+    double mynum2= mymethod(12.234, 1.234);
 
     System.out.println("int: " +mynum1);
     System.out.println("double: " +mynum2);
 }
     
 }
+
