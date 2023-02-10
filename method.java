490c314602ba343480ea2e219ec6b1756228afe4
Ankit Soni <98032129+Ankit-sony@users.noreply.github.com>
2023-02-10 10:01:59 +0000

    method parameter

--- method.java
+++ method.java
@@ -7,18 +7,40 @@
 //     myMethod();
 //   }
 // }
-public class method{
-    static void secondMethod() {
-        System.out.println("second method");
-    }
-    static void thirdMethod(){
-        System.out.println("thirdmethod");
 
+// ------------------------------------------------------------one-----------------------
+
+
+
+// public class method{
+//     static void secondMethod() {
+//         System.out.println("second method");
+//     }
+//     static void thirdMethod(){
+//         System.out.println("thirdmethod");
+
+//     }
+
+//     public static void main(String[] args){
+//         secondMethod();
+//         thirdMethod();
+//     }
+
+// }
+
+
+
+// --------------------------------------java parameters----------------------
+
+
+public class method{
+    static void myMethod(String fname){
+        System.out.println(fname +" Superstar");
     }
 
     public static void main(String[] args){
-        secondMethod();
-        thirdMethod();
+        myMethod("Danish");
     }
-
 }
+
+
